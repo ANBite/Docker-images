99d5affe92a40896ecf443d18314ad491c47bb60
ANBite <124651559+ANBite@users.noreply.github.com>
2025-10-07 22:33:09 -0600

    create migration

--- Peluches/peluchitos/settigns.py
+++ Peluches/peluchitos/settigns.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'peluchitos',  # 👈 agrega esta línea
+    'Peluches', 
 ]
 
 
