603ceb2ddc297cc5b6f0f9a2a5c6f17cc139314b
ANBite <124651559+ANBite@users.noreply.github.com>
2025-10-07 22:22:50 -0600

    create model 1

---
+++ Peluches/peluchitos/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PeluchitosConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'peluchitos'
---
+++ Peluches/peluchitos/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# 🧍‍♀️ Cliente
+class Cliente(models.Model):
+    nombre = models.CharField(max_length=100)
+    correo = models.EmailField(unique=True)
+    telefono = models.CharField(max_length=20, blank=True)
+    direccion = models.CharField(max_length=200, blank=True)
+
+    def __str__(self):
+        return self.nombre
