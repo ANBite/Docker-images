dd30e99ef045011e7cae91e74ff3e84f0dc0d8b6
ANBite <124651559+ANBite@users.noreply.github.com>
2025-10-07 22:24:03 -0600

    create model 2

--- Peluches/peluchitos/models.py
+++ Peluches/peluchitos/models.py
@@ -9,3 +9,15 @@ class Cliente(models.Model):
 
     def __str__(self):
         return self.nombre
+    
+
+
+# 🧸 Peluche
+class Peluche(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField(blank=True)
+    precio = models.DecimalField(max_digits=8, decimal_places=2)
+    stock = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.nombre} - Q{self.precio}"
